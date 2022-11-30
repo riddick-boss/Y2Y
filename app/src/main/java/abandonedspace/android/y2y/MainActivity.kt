00c29b5a5905ba0cdd911412928b9c3d30dcda41
Pawel Kremienowski <Kremienowski33@gmail.com>
2022-11-30 20:44:33 +0100

    Initial commit

---
+++ app/src/main/java/abandonedspace/android/y2y/MainActivity.kt
@@ -0,0 +1,43 @@
+package abandonedspace.android.y2y
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import abandonedspace.android.y2y.ui.theme.Y2YTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Y2YTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Y2YTheme {
+        Greeting("Android")
+    }
+}
